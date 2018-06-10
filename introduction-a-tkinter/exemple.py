09df8b3e90280901739a7664e127783f5b70bb71
Luc Bertrand <luc.bertrand@programmer.net>
2018-06-10 12:36:09 +0200

    Update exemple.py

--- introduction-a-tkinter/exemple.py
+++ introduction-a-tkinter/exemple.py
@@ -1,3 +1,5 @@
+"""Application de conversion en binaire avec interface graphique."""
+
 import Tkinter as tk
 
 fenetre = tk.Tk()
@@ -10,10 +12,10 @@ reponse = tk.Label(fenetre, text='0b')
 reponse.pack()
 
 def convertir():
+	"""Convertir en binaire le nombre entré et afficher le résultat."""
 	reponse.config(text=bin(int(entree.get())))
 
-bouton = tk.Button(fenetre, text='Convertir',
-		   command=convertir)
+bouton = tk.Button(fenetre, text='Convertir', command=convertir)
 bouton.pack()
 
 fenetre.mainloop()
